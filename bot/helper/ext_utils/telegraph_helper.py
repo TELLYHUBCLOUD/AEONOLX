3b9bfbe567b728b543a9a3449bf2c55bc8ecf9ef
TELLYHUBCLOUD <147921265+TELLYHUBCLOUD@users.noreply.github.com>
2024-07-11 21:25:46 +0530

    Update telegraph_helper.py

--- bot/helper/ext_utils/telegraph_helper.py
+++ bot/helper/ext_utils/telegraph_helper.py
@@ -13,8 +13,8 @@ class TelegraphHelper:
         self.telegraph = Telegraph(domain='graph.org')
         self.short_name = token_hex(4)
         self.access_token = None
-        self.author_name = 'Aeon'
-        self.author_url = 'https://t.me/ProjectAeon'
+        self.author_name = 'TELLYCLOUD BOTS'
+        self.author_url = 'https://t.me/TELLYCLOUD_BOTS'
 
     async def create_account(self):
         await self.telegraph.create_account(
