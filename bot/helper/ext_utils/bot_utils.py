09704d696c9ee94f9af80874c6b16d526bca91a9
TELLYHUBCLOUD <147921265+TELLYHUBCLOUD@users.noreply.github.com>
2024-07-13 12:48:43 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -212,7 +212,7 @@ def get_readable_message():
         globals()['STATUS_START'] = STATUS_LIMIT * (PAGES - 1)
         globals()['PAGE_NO'] = PAGES
     for download in list(download_dict.values())[STATUS_START:STATUS_LIMIT+STATUS_START]:
-        msg += f"<b>📂 Filename:</b> {escape(f'{download.name()}')}\n"
+        msg += f"<blockquote><b>📂 Filename:</b> {escape(f'{download.name()}')}</blockquote>\n"
         msg += f"<b>👤 Name:</b> {source(download)}\n\n"
         msg += f"<b>    {download.status()}...</b>"
         if download.status() not in [MirrorStatus.STATUS_SPLITTING, MirrorStatus.STATUS_SEEDING, MirrorStatus.STATUS_PROCESSING]:
